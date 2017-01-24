f3649e8e2fe34a4f8dfd6b903b24f97e9c122408
Arman Kazemi <kas1er@hotmail.com>
2017-01-24 22:52:32 +0000

    character counter changes with text input and font color goes red below 10

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,1 +1,15 @@
-console.log("compose-char-counter.js");
+$(document).ready(function() {
+  $(".new-tweet textarea").on("keyup", function() {
+    const charLim = 140;
+    const span = $(this).nextAll('span');
+    let len = charLim - $(this).val().length;
+    if(len < 10) {
+      span.css("color", "red");
+    } else {
+      span.css("color", "black");
+    }
+    span.text(len);
+  });
+});
+
+
