cf24845be5b3f351db340a3a1bf520dfaeff2ff3
Arman Kazemi <kas1er@hotmail.com>
2017-01-25 03:26:53 +0000

    tweet container set with css and hover-over effects

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".new-tweet textarea").on("keyup", function() {
+  $(".new-tweet textarea").on("input", function() {
     const charLim = 140;
     const span = $(this).nextAll('span');
     let len = charLim - $(this).val().length;
